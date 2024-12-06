7324f56d825f257833e4842da9d3432fef056c05
Vasiliy <Ruyk17@gmail.com>
2024-12-06 17:38:13 +0300

    InitialChange

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,33 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void ShouldNeedToBuy(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1_900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldNotNeedToBuy(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2_000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+
+        assertEquals(expected, actual);
+    }
+
+}
